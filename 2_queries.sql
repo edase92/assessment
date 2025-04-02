231f01b8a74444bd8283506dd126da95f483e433
edase92 <dase.etienne92@gmail.com>
2025-04-02 23:00:33 +0200

    Create 2_queries.sql

---
+++ 2_queries.sql
@@ -0,0 +1,37 @@
+WITH join_table AS ( 
+  SELECT
+    u.id AS user_id, 
+    first_name, 
+    last_name, 
+    user_group, 
+    e.id, 
+    event_name, 
+    date AS event_date
+  FROM
+    `fourth-sphere-360919.bewerberaufgaben.user_data` u
+  LEFT JOIN
+    `fourth-sphere-360919.bewerberaufgaben.event_data` e
+    ON u.id = e.user_id
+), daily_counts AS (
+  SELECT
+    DISTINCT CONCAT(user_id, ' - ', first_name, ' ', last_name) AS full_id,
+    event_date,
+    COUNTIF(event_name = 'page_view') AS page_views
+  FROM
+    join_table
+  GROUP BY
+    1, 2
+  ORDER BY
+    1, 2
+), daily_counts_user_groups AS (
+  SELECT
+    user_group,
+    event_date,
+    COUNTIF(event_name = 'page_view') AS page_views
+  FROM
+    join_table
+  GROUP BY
+    1, 2
+  ORDER BY
+    1, 2
+)
