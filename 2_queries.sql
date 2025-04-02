6806db1f0ecaac291550f5745d3fa2258350c174
edase92 <dase.etienne92@gmail.com>
2025-04-02 23:32:43 +0200

    Update 2_queries.sql

--- 2_queries.sql
+++ 2_queries.sql
@@ -85,3 +85,51 @@ GROUP BY
   1
 
 -- Aufgabe 1. b)
+-- Variante 1 (Nutzer oder Tage, mit mehr als zwei page views)
+SELECT 
+  DISTINCT CONCAT(user_id, ' - ', first_name, ' ', last_name) AS dimension, 
+  COUNT(event_name) AS count_page_views 
+FROM join_table 
+  WHERE event_name = 'page_view' 
+GROUP BY 
+  dimension
+HAVING 
+  COUNT(event_name) >= 2
+UNION ALL
+SELECT 
+  CAST(event_date AS STRING) AS dimension, 
+  COUNT(event_name) AS count_page_views 
+FROM join_table 
+WHERE 
+  event_name = 'page_view' 
+GROUP BY dimension 
+HAVING 
+  COUNT(event_name) >= 2
+
+-- Aufgabe 1. b)
+-- Variante 2 (Nutzer und Tage, mit mehr als zwei page views)
+SELECT
+  DISTINCT CONCAT(user_id, ' - ', first_name, ' ', last_name) AS full_id,
+  EXTRACT(DAY FROM event_date) AS day_of_month,
+  COUNTIF(event_name = 'page_view') AS page_views
+FROM
+  join_table
+GROUP BY
+  1, 2
+HAVING
+  page_views >= 2
+ORDER BY
+  1 ASC, day_of_month ASC
+
+-- Aufgabe 2
+SELECT
+  user_group, 
+  ARRAY_AGG(event_date IGNORE NULLS ORDER BY event_date ASC) AS event_date, 
+  ARRAY_AGG(page_views ORDER BY event_date ASC) AS page_views, 
+  SUM(page_views) AS total_page_views
+FROM
+  daily_counts_user_groups
+WHERE
+  event_date IS NOT NULL
+GROUP BY
+  1 
